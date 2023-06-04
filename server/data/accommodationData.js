297f558659993affc040425a22498e35350778fd
Luana <luanamartiins18@gmail.com>
2023-06-04 15:19:38 -0300

    atualizacao accommodation minas_gerais

--- server/data/accommodationData.js
+++ server/data/accommodationData.js
@@ -1,16 +1,16 @@
 // RIO DE JANEIRO 
 export const riodejaneiroAccommodations = [
     {
-        image: "assets/hotel/alagoas/hotel-costamar/background-hotel-windson.png",
-        images: ["assets/hotel/alagoas/hotel-costamar/background-hotel-windson.png", "assets/hotel/alagoas/hotel-costamar/alimentos.png", "assets/hotel/alagoas/hotel-costamar/area.png", "assets/hotel/alagoas/hotel-costamar/piscina.png", "assets/hotel/alagoas/hotel-costamar/quarto.png"],
+        image: "assets/hotel/rio-janiero/windson/background-hotel-windson.png",
+        images: ["assets/hotel/rio-janeiro/windson/background-hotel-windson.png", "assets/hotel/rio-janeiro/windson/alimentos.png", "assets/hotel/rio-janeiro/windson/area.png", "assets/hotel/alagoas/hotel-costamar/piscina.png", "assets/hotel/rio-janeiro/windson/quarto.png"],
         nomeLocal: 'Windson Plaza Copacabana',
         localizacao: 'Copacabana, Rio de Janeiro',
         localizacaoCompleta: 'Avenida Princesa Isabel 263 Copacabana, Rio de Janeiro, Estado do Rio de Janeiro 22011-011 Brasil',
         informacaoGeral: 'A 350 metros da praia',
         tipoQuarto: 'Quarto Duplo Superior',
         quartos: [{
-            image: "assets/hotel/alagoas/hotel-costamar/background-hotel-windson.png",
-            imageQuarto: "assets/hotel/alagoas/hotel-costamar/quarto.png",
+            image: "assets/hotel/rio-janeiro/windson/background-hotel-windson.png",
+            imageQuarto: "assets/hotel/rio-janeiro/windson/quarto.png",
             nomeLocal: 'Windson Plaza Copacabana',
             localizacao: 'Copacabana, Rio de Janeiro',
             tipoQuarto: 'Standard',
@@ -19,9 +19,9 @@ export const riodejaneiroAccommodations = [
             adicional: 'Café da Manhã',
             id: 1
         }, {
-            image: "assets/hotel/alagoas/hotel-costamar/background-hotel-windson.png",
+            image: "assets/hotel/rio-janeiro/windson/background-hotel-windson.png",
             nomeLocal: 'Windson Plaza Copacabana',
-            imageQuarto: "assets/hotel/alagoas/hotel-costamar/quarto.png",
+            imageQuarto: "assets/hotel/rio-janeiro/windson/quarto.png",
             localizacao: 'Copacabana, Rio de Janeiro',
             tipoQuarto: 'King',
             capacidade: '2 Adultos',
@@ -36,7 +36,7 @@ export const riodejaneiroAccommodations = [
         informacoesAdicionais: ['Animais de estimação permitidos', 'Acesso Wi-Fi gratuito', 'Ar Condicionado', 'Casa de banho privativa', 'Recepção disponível 24 horas', 'Cartão de acesso', 'Cofre', 'Sala para Bagagem'],
         valor: 402,
         identificador: 1,
-        geoLocalizacao: ['-22.970046413614764', '-43.18423807246657']
+        geoLocalizacao: ['-22.961837144717727, -43.175058']
     },
     {
         image: "assets/hotel/rio-janeiro/novotel/background-hotel-novotel.png",
@@ -80,6 +80,85 @@ export const riodejaneiroAccommodations = [
         geoLocalizacao: ['-22.945363660153305', '-43.18297473199225']
     }
 ]
+export const minasgeraisAccommodations = [
+    {
+        image: "assets/hotel/minas-gerais/normandy-hotel/normandy-fachada.png",
+        images: ["assets/hotel/minas-gerais/normandy-hotel/normandy-academia.png", "assets/hotel/minas-gerais/normandy-hotel/normandy-cafe.png", "assets/hotel/minas-gerais/normandy-hotel/piscina.png", "assets/hotel/minas-gerais/normandy-hotel/normandy-quarto.png", "assets/hotel/minas-gerais/normandy-hotel/fachada.png"],
+        nomeLocal: 'Normandy hotel',
+        localizacao: 'Centro, Belo Horizonte',
+        localizacaoCompleta: 'Rua dos Tamoios, 212 , Centro de Belo Horizonte, Belo Horizonte, CEP 30120-050, Brasil',
+        informacaoGeral: 'Otima localização',
+        tipoQuarto: 'Quarto Duplo Standart',
+        quartos: [{
+            image: "assets/hotel/minas-gerais/normany-hotel/normandy-fachada.png",
+            imageQuarto: "assets/hotel/minas-gerais/normandy-hotel/normandy-quarto.png",
+            nomeLocal: 'Normandy hotel',
+            localizacao: 'Centro,  Belo Horizonte',
+            tipoQuarto: 'Standard',
+            capacidade: '2 Adultos',
+            valor: 350,
+            adicional: 'Café da Manhã',
+            id: 1
+        }, {
+            image: "assets/hotel/minas-gerais/normandy-hotel/normandy-fachada.png",
+            nomeLocal: 'Normandy hotel',
+            imageQuarto: "assets/hotel/minas-gerais/normandy-hotel/normandy-quarto.png",
+            localizacao: 'Centro, Belo Horizonte',
+            tipoQuarto: 'King',
+            capacidade: '2 Adultos e 1 criança',
+            valor: 450,
+            adicional: 'Meia Pensão',
+            id: 2
+        }],
+        descricao: ' Localizado em Belo Horizonte, o Normandy Hotel dispõe de um restaurante e um business center. A propriedade está situada em frente ao complexo da Igreja de São José.',
+        camas: '1 cama de casal',
+        avaliacao: '7,1',
+        tempoCapacidade: '1 noite, 2 adultos',
+        informacoesAdicionais: ['Animais de estimação permitidos', 'Acesso Wi-Fi gratuito', 'Ar Condicionado', 'Casa de banho privativa', 'Recepção disponível 24 horas', 'Cartão de acesso', 'Cofre', 'Sala para Bagagem'],
+        valor: 350,
+        identificador: 1,
+        geoLocalizacao: ['-19.919400302415653, -43.93854796137455']
+    },
+    {
+        image: "assets/hotel/minas-gerais/savassi-hotel/savassi-frente.png",
+        images: ["assets/hotel/minas-gerais/savassi-hotel/savassi-frente.png", "assets/hotel/minas-gerais/savassi-hotel/savassi-banheiro.png", "assets/hotel/minas-gerais/savassi-hotel/savassi-cafe.png", "assets/hotel/minas-gerais/savassi-hotel/quarto.png", "assets/hotel/minas-gerais/savassi-hotel/savassi-restaurante.png"],
+        nomeLocal: 'Savassi Hotel',
+        localizacao: 'Savassi, Belo Horizonte',
+        localizacaoCompleta: 'Rua Sergipe, 939 , Savassi, Belo Horizonte, CEP 30130-171, Brasil',
+        informacaoGeral: 'Otima lovalização',
+        tipoQuarto: 'Quarto simples',
+        quartos: [{
+            image: "assets/hotel/minas-gerais/savassi-hotel/savassi-frente.png",
+            imageQuarto: "assets/hotel/minas-gerais/savassi-hotel/savassi-quarto.png",
+            nomeLocal: 'Savassi Hotel',
+            localizacao: 'Savassi, Belo Horizonte',
+            tipoQuarto: 'Standard casal',
+            capacidade: '2 Adultos',
+            valor: 209,
+            adicional: 'Café da Manhã',
+            id: 1
+        }, {
+            image: "assets/hotel/minas-gerais/savassi-hotel/savassi-frente.png",
+            imageQuarto: "assets/hotel/minas-gerais/savassi-hotel/savassi-quarto.png",
+            nomeLocal: 'Savassi Hotel',
+            localizacao: 'Savassi, Belo Horizonte',
+            tipoQuarto: 'camas separadas',
+            capacidade: '2 Adultos',
+            valor: 200,
+            adicional: 'Meia Pensão',
+            id: 2
+        }],
+        descricao: 'A apenas 4 minutos do centro da cidade de Belo Horizonte, este hotel fica na badalada área de Savassi. A acomodação dispõe de uma piscina na cobertura e um restaurante regional. O Wi-Fi e o estacionamento são gratuitos.' ,
+        camas: '1 cama de casal',
+        avaliacao: '7,0',
+        tempoCapacidade: '1 noite, 2 adultos',
+        informacoesAdicionais: ['Animais de estimação permitidos', 'Acesso Wi-Fi gratuito', 'Ar Condicionado', 'Casa de banho privativa', 'Recepção disponível 24 horas', 'Cartão de acesso', 'Cofre', 'Sala para Bagagem'],
+        valor: 209,
+        identificador: 2,
+        geoLocalizacao: ['-19.93395361957372, -43.937319546030906']
+    }
+]
+
 
 // Alagoas
 
